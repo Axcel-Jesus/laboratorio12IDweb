48913d8fcc1cd526ffbd897bb348467f5375fcaa
Axcel-Jesus <acunol@unsa.edu.pe>
2025-10-28 17:29:30 -0500

    finalizado

--- funcion1.js
+++ funcion1.js
@@ -151,8 +151,18 @@ function contador(){
         return contador;
     }
 }
+while(true){
+    let numero=parseInt(promt("que quieres hacer=\n1.incrementar\n2.resetear"));
+    if(numero==1){
+        console.log(contador.incrementar());
+    }else if(numero==2){
+        console.log(contador.resetear());
+        break;
+    }
+}
+//la funcion realiza un incremento o reseteo del contador, y cuando resetea, termina la ejecucion del programa
 
-
+//problema 14
 function acumulador(){
     let acumulador=0;
     return function(n){
@@ -160,11 +170,25 @@ function acumulador(){
         return acumulador;
     }
 }
+while(true){
+    let numero=parseInt(promt("que quieres hacer=\n1.acumular\n2. salir"));
+    if(numero==1){
+        console.log(acumulador(prompt("ingrese un numero")));
+    }else if(numero==2){
+        break;
+    }
+}
+//la funcion realiza una acumulacion hasta que se salga del bucle
 
+//problema 15
 function saludando(name="amigo"){
     return 'hola, ${name}';
 }
+console.log(saludando(prompt("ingrese el nombre que quieres saludar")));
+//la funcion devuelve un saludo segun el nombre ingresado
 
+
+//problema 16
 function media(...numeros){
     let total=0;
     for(let i=0;i<numeros.length;i++){
@@ -172,12 +196,35 @@ function media(...numeros){
     }
     return total/numeros.length;
 }
+let numeros=new Array();
+while(true){
+    let numero=parseInt(prompt("ingrese un numero que quieres agregar a la media"));
+    if(numero==0){
+        break;
+    }else{
+        numeros.push(numero);
+    }
+}
+console.log(media(...numeros)+"es la media de los numeros ingresados");
+//la funcion devuelve la media de los numeros ingresados
 
+//problema 17
 function mostrarDatos(nombre, edad, ...hobbies){
     console.log(`Hola, ${nombre}! Tu edad es ${edad} y tus hobbies son ${hobbies.join(", ")}`);
 }
-mostrarDatos("Juan", 25, "programar", "jugar");
+let hobis=new Array();
+while(true){
+    let hobby=prompt("ingrese un hobby que quieres agregar a la lista");
+    if(hobby==0){
+        break;
+    }else{
+        hobis.push(hobby);
+    }
+}
+console.log(mostrarDatos(prompt("ingrese el nombre"),prompt("ingrese la edad"),...hobis));
+//la funcion devuelve un mensaje con el nombre, edad y hobbies ingresados
 
+//problema 18
 function ejecutarOperacion(operacion, a, b){
     switch(operacion){
         case "+":
@@ -190,7 +237,23 @@ function ejecutarOperacion(operacion, a, b){
             return a/b;
     }
 }
+switch(promt("ingrese la operacion que quieres realizar:\n+\n-\n*\n/")){
+    case "+":
+        console.log(ejecutarOperacion(promt("ingrese la operacion que quieres realizar"),promt("ingrese el primer numero"),promt("ingrese el segundo numero"))+"es el resultado de la operacion ingresada");
+        break;
+    case "-":
+        console.log(ejecutarOperacion(promt("ingrese la operacion que quieres realizar"),promt("ingrese el primer numero"),promt("ingrese el segundo numero"))+"es el resultado de la operacion ingresada");
+        break;
+    case "*":
+        console.log(ejecutarOperacion(promt("ingrese la operacion que quieres realizar"),promt("ingrese el primer numero"),promt("ingrese el segundo numero"))+"es el resultado de la operacion ingresada");
+        break;
+    case "/":
+        console.log(ejecutarOperacion(promt("ingrese la operacion que quieres realizar"),promt("ingrese el primer numero"),promt("ingrese el segundo numero"))+"es el resultado de la operacion ingresada");
+        break;
+}
+//la funcion devuelve el resultado de la operacion ingresada y los numeros, las operaciones son funciones flechas dentro de la funcion operacionesMatematicas
 
+//problema 19
 function filtrarArreglo(arreglo, callback){
     let nuevoArreglo=[];
     for(let i=0;i<arreglo.length;i++){
@@ -199,13 +262,18 @@ function filtrarArreglo(arreglo, callback){
         }
     }
 }
+//la funcion devuelve un nuevo arreglo con los elementos que cumplen la condicion
 
+
+//problema 20
 function descargarArchivo(url, callback){
     console.log("descargando...");
     callback();
     console.log("descarga completa");
 }
+//la funcion devuelve el estado de descarga del archivo
 
+//problema 21
 function potenciaRecursiva(base, exponente){
     if(exponente===0){
         return 1;
@@ -213,7 +281,12 @@ function potenciaRecursiva(base, exponente){
         return base*potenciaRecursiva(base, exponente-1);
     }
 }
+console.log(potenciaRecursiva(prompt("ingrese el numero que quieres calcular la potencia"),prompt("ingrese el exponente"))+"es la potencia");
+//la funcion devuelve la potencia de un numero de forma recursiva
 
+
+
+//problema 22
 function secuenciaAritmetica(inicio, paso){
     let numero=inicio;
     return function(){
@@ -221,4 +294,5 @@ function secuenciaAritmetica(inicio, paso){
         return numero;
     }
 }
-
+console.log(secuenciaAritmetica(prompt("ingrese el numero que quieres comenzar"),prompt("ingrese el paso"))()+"es la secuencia aritmetica");
+//la funcion devuelve la secuencia aritmetica tambien de forma recursiva
